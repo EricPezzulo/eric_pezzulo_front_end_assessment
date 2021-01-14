91dc00478e95af9918934fe141def5b8f358cbde
Eric Pezzulo <eric.pezzulo@gmail.com>
2021-01-13 19:44:37 -0500

    ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import styles from './App.module.css';
+import axios from 'axios';
 
 function App() {
+  
+  const [info, setInfo]= useState([])
+
+  const url = 'https://api.hatchways.io/assessment/students'
+
+
+  useEffect(() => {
+    const fetchInfo = async () => {
+      const result = await axios(url)
+
+      console.log(result.data)
+      setInfo(result.data)
+    }
+    fetchInfo()
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      
+      
+        {info.map(students => (
+          <p>{students.city}</p>
+          
+        ))} 
+  
+        
+    
     </div>
-  );
+  
+  )
 }
 
 export default App;
